6f95e99da26191e3e0d76648f8864df7db66126d
丁少华 <960423114@qq.com>
2023-12-11 19:36:18 +0800

    feat: 优化css

--- src/editor.tsx
+++ src/editor.tsx
@@ -5,13 +5,11 @@ import defaultConf from "./default-conf";
 import { textSelect } from "./helper";
 import React, { useEffect, useRef } from "react";
 
-
 const md = MarkdownIt({
   html: true,
   linkify: true,
   typographer: true,
 });
-
 
 function MdEditor(props) {
   const finalToolbar = [
@@ -25,47 +23,52 @@ function MdEditor(props) {
 
   const mdEditorNode = useRef(null);
 
-  useEffect(()=>{
-    if(mdEditorNode.current){
-      props.editor.setElement(mdEditorNode.current)
+  useEffect(() => {
+    if (mdEditorNode.current) {
+      props.editor.setElement(mdEditorNode.current);
     }
-  },[mdEditorNode])
+  }, [mdEditorNode]);
 
   return (
     <div className="md_editor" ref={mdEditorNode}>
-      <div className="toolbar">
-        {finalToolbar.map((item) =>
-          item.render ? (
-            <div className="item" key={item.id}>
-             <item.render editor={props.editor}/>
-            </div>
-          ) : (
-            <div
-              className="item"
-              key={item.id}
-              onClick={() => {
-                item.action(props.editor);
-              }}
-            >
-              <img src={item.icon} />
-            </div>
-          )
-        )}
-      </div>
+      {!props.editor.readonly && (
+        <div className="toolbar">
+          {finalToolbar.map((item) =>
+            item.render ? (
+              <div className="item" key={item.id}>
+                <item.render editor={props.editor} />
+              </div>
+            ) : (
+              <div
+                className="item"
+                key={item.id}
+                onClick={() => {
+                  item.action(props.editor);
+                }}
+              >
+                <img src={item.icon} />
+              </div>
+            )
+          )}
+        </div>
+      )}
+
       <div className="content">
-        <textarea
-          onSelect={(e) => {
-            e.stopPropagation();
-            props.editor.setSelection(textSelect(e));
-          }}
-          value={props.editor.content}
-          onInput={({ target}) => {
-            props.editor.setContent(target.value);
-            target.style.height = "inherit";
-            target.style.height = `${target.scrollHeight}px`;;
-            
-          }}
-        />
+        {!props.editor.readonly && (
+          <textarea
+            onSelect={(e) => {
+              e.stopPropagation();
+              props.editor.setSelection(textSelect(e));
+            }}
+            value={props.editor.content}
+            onInput={({ target }) => {
+              props.editor.setContent(target.value);
+              target.style.height = "inherit";
+              target.style.height = `${target.scrollHeight}px`;
+            }}
+          />
+        )}
+
         <RenderRtf {...renderRtfProps} />
       </div>
     </div>
